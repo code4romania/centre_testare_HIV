4a785ffdc23807d7b3c406566c8db840a00a954d
vladplesu <vladp1987@gmail.com>
2022-02-03 08:01:22 +0200

    remove guide page

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,11 @@ import { i18n } from '@lingui/core';
 
 import Home from './containers/home';
 import About from './containers/about';
-import Guide from './containers/guide';
 import Blog from './containers/blog';
 import BlogPost from './containers/blog-post';
 import Terms from './containers/Terms';
 import Policy from './containers/Policy';
 import NotFound from './containers/404/404';
-import AddBuilding from './containers/add-building';
 import ThankYou from './containers/thankyou';
 import ScrollToTop from './components/ScrollToTop';
 
@@ -36,9 +34,6 @@ const App = () => {
           <Route path="/despre">
             <About />
           </Route>
-          <Route path="/ghid">
-            <Guide />
-          </Route>
           <Route exact path="/blog">
             <Blog />
           </Route>
@@ -50,9 +45,6 @@ const App = () => {
           </Route>
           <Route path="/politica-de-confidentialitate">
             <Policy />
-          </Route>
-          <Route path="/adauga-cladire">
-            <AddBuilding />
           </Route>
           <Route path="/multumim">
             <ThankYou />
