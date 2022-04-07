f4ec5739b33c1b4d3e25745fa28623fb83edf3a2
Tudor <tudor.amariei@code4.ro>
2022-04-08 00:00:42 +0300

    Add import-export to tags (#69)

--- api/blog/admin.py
+++ api/blog/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from taggit.admin import TagAdmin
+from taggit.models import Tag
 
 from .models import Post
 
 
+@admin.register(Post)
 class PostAdmin(ImportExportModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ("title", "slug", "created", "published", "is_visible")
@@ -27,4 +30,9 @@ class PostAdmin(ImportExportModelAdmin):
     ]
 
 
-admin.site.register(Post, PostAdmin)
+class TagAdminWithImportExport(ImportExportModelAdmin, TagAdmin):
+    ...
+
+
+admin.site.unregister(Tag)
+admin.site.register(Tag, TagAdminWithImportExport)
