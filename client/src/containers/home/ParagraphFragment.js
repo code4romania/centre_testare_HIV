c3ded4ac14ce47869f76ad80cbaf55b1022dbf9d
vladplesu <vladp1987@gmail.com>
2022-04-07 09:50:05 +0300

    update home page

--- client/src/containers/home/ParagraphFragment.js
+++ client/src/containers/home/ParagraphFragment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Icon, Typography } from 'antd';
 import { Trans } from '@lingui/macro';
+import { Link } from 'react-router-dom';
 
 const { Paragraph, Title } = Typography;
 
@@ -14,30 +15,24 @@ export default () => {
         </Title>
       </Row>
       <Row gutter={[0, 32]} type="flex" justify="space-between">
-        <Col sm={24} md={11}>
-          {/* @TODO replace text + translate */}
-          <Paragraph ellipsis={{ rows: 10, expandable: true }}>
-            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-            default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-            their infancy. Various versions have evolved over the years, sometimes by accident,
-            sometimes on purpose (injected humour and the like) Many desktop publishing packages and
-            web page editors now use Lorem Ipsum as their default model text, and a search for
-            'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-            evolved over the years, sometimes by accident, sometimes on purpose (injected humour and
-            the like).
-          </Paragraph>
-        </Col>
-        <Col sm={24} md={11}>
-          {/* @TODO replace text + translate */}
-          <Paragraph ellipsis={{ rows: 10, expandable: true }}>
-            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-            default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-            their infancy. Various versions have evolved over the years, sometimes by accident,
-            sometimes on purpose (injected humour and the like) Many desktop publishing packages and
-            web page editors now use Lorem Ipsum as their default model text, and a search for
-            'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-            evolved over the years, sometimes by accident, sometimes on purpose (injected humour and
-            the like)..
+        <Col>
+          <Paragraph>
+            <Trans>
+              Proiectul este derulat de Asociația Semper Musica în parteneriat cu Federația
+              Asociațiilor Studenților în Medicină din România și cu Sex and society și beneficiază
+              de o finanțare în valoare de 𝟮𝟲𝟯.𝟲𝟰𝟬,𝟱𝟬 €, prin programul Active Citizens Fund
+              România, finanțat de Islanda, Liechtenstein și Norvegia prin Granturile SEE 2014-2021.
+              Conținutul acestui website nu reprezintă în mod necesar poziția oficială a Granturilor
+              SEE și Norvegiene 2014-2021; pentru mai multe informații accesați{' '}
+              <Link to={{ pathname: 'https://eeagrants.org' }} target="_blank">
+                www.eeagrants.org
+              </Link>
+              . Informații despre Active Citizens Fund România sunt disponibile la
+              <Link to={{ pathname: 'htpps://activecitizensfund.ro' }} target="_blank">
+                www.activecitizensfund.ro
+              </Link>
+              .
+            </Trans>
           </Paragraph>
         </Col>
       </Row>
